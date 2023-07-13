96ed422f343b8a13e8a2e00d1514d0c7e6805a33
tyAlexios <97611066+tyAlexios@users.noreply.github.com>
2023-07-13 17:20:44 +0800

    Update CapVideos.py

--- CapVideos.py
+++ CapVideos.py
@@ -4,11 +4,8 @@ import time
 
 import cv2
 
-import backend
-from threading import Thread
-
 def make_parser():
-    parser = argparse.ArgumentParser("Demo!")
+    parser = argparse.ArgumentParser("Simultaneous Multi-Video Capture!")
 
     parser.add_argument(
         "--NumOfCam", type=int, default=0, help="the number of used cameras"
@@ -32,31 +29,12 @@ def make_parser():
     
     return parser
 
-def SaveVideos(n, caps, vid_writers, logfile):
-    base_a_coor = (0., 0., 0.)
-    base_b_coor = (3.495, 0., 0.)
-    base_c_coor = (0., 6.580, 0.)
-    
-    lt = backend.lighTagAlgo()
-    lt.serialConnect()
-    
-    lt.setBaseACoor(*base_a_coor)
-    lt.setBaseBCoor(*base_b_coor)
-    lt.setBaseCCoor(*base_c_coor)
-    
-    def backend_loop():
-        while 1:
-            time.sleep(0.1)
-            pos = lt.run()
-            
-    backend_thread = Thread(target=backend_loop, name="BackendThread")
-    backend_thread.daemon = True
-    backend_thread.start()
+def SaveVideos(n, caps, vid_writers):
     
     frame_id = 0
     frames = []
     
-    skip_frames = 0
+    skip_frames = 30
     
     while True:
         frame_id += 1
@@ -77,10 +55,6 @@ def SaveVideos(n, caps, vid_writers, logfile):
         for i in range(n):
             cv2.imshow("view{0}".format(i), frames[i])
             vid_writers[i].write(frames[i])
-
-        coordinate = lt.getCoor()
-        print("{0:5} {1:.6f} {2:.6f}\n".format(frame_id, coordinate[0], coordinate[1]))
-        logfile.write("{0} {1:.6f} {2:.6f}\n".format(frame_id, coordinate[0], coordinate[1]))
         
         ch = cv2.waitKey(1)
         if ch == 27 or ch == ord("q") or ch == ord("Q"):
@@ -90,7 +64,7 @@ def SaveVideos(n, caps, vid_writers, logfile):
         
 def main(args):
     current_time = time.localtime()
-    vis_folder = os.path.join("./outputs", "track_vis")
+    vis_folder = os.path.join("outputs", "track_vis")
     os.makedirs(vis_folder, exist_ok=True)
     timestamp = time.strftime("%Y_%m_%d_%H_%M_%S", current_time)
     save_folder = os.path.join(vis_folder, timestamp)
@@ -114,12 +88,7 @@ def main(args):
             os.path.join(save_folder, "camera{0}.mp4".format(i)), cv2.VideoWriter_fourcc(*'mp4v'), caps[i].get(cv2.CAP_PROP_FPS), (int(caps[i].get(cv2.CAP_PROP_FRAME_WIDTH)), int(caps[i].get(cv2.CAP_PROP_FRAME_HEIGHT)))
         ))
     
-    logfile = open(os.path.join(save_folder, "log.txt"), "a+")
-    logfile.write("frame_id    X    Y\n")
-    
-    SaveVideos(NUM_CAM, caps, vid_writers, logfile)
-    
-    logfile.close()
+    SaveVideos(NUM_CAM, caps, vid_writers)
 
 if __name__ == "__main__":
     args = make_parser().parse_args()
